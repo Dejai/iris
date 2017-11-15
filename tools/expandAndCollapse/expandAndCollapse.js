9c8f18ef726fe6d0b2160684f6981eb032c8eb7b
Derrick Fyfield <Dejai@users.noreply.github.com>
2017-11-15 12:55:50 -0500

    Update expandAndCollapse.js

--- tools/expandAndCollapse/expandAndCollapse.js
+++ tools/expandAndCollapse/expandAndCollapse.js
@@ -7,9 +7,11 @@ function expand(){
 	document.getElementById("expander").classList.remove("selectedOption");
 	document.getElementById("expander").blur();
 	document.getElementById("collapser").classList.add("selectedOption");
-	for (var x = 0; x < elements.length; x++){ 
-		elements[x].childNodes[0].childNodes[0].classList.add("expanded");
-		elements[x].childNodes[1].classList.remove("expand-hidden");	
+	for (var x = 0; x < elements.length; x++){
+		elements[x].querySelectorAll("expand-control")[0].classList.add("expanded");
+		//elements[x].childNodes[0].childNodes[0].classList.add("expanded");
+		//elements[x].childNodes[1].classList.remove("expand-hidden");	
+		elements[x].querySelectorAll("expand-hidden")[0].classList.remove("epxand-hidden");
 	}
 }
 function collapse(){
