720713c0b4c3b9d2b129e28ff52ba76fde850f81
Derrick Fyfield <Dejai@users.noreply.github.com>
2017-12-12 12:50:10 -0500

    Update signupApp2.js

--- pages/signupApp2/assets/signupApp2.js
+++ pages/signupApp2/assets/signupApp2.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var homeButton = document.getElementById("homeButton");
-	homeButton.href = "/iris/pages/signupApp2/";
+	homeButton.href = "/iris/pages/signupApp2/examples";
 });
